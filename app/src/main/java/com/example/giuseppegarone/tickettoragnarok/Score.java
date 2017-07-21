8414e5ac1971e6bc3476ab55af1fabee243c5492
melissacoarezza <s231688@studenti.polito.it>
2017-07-21 18:03:26 +0200

    scrittura database_sezione Classifica

---
+++ app/src/main/java/com/example/giuseppegarone/tickettoragnarok/Score.java
@@ -0,0 +1,28 @@
+package com.example.giuseppegarone.tickettoragnarok;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by melissacoarezza on 21/07/17.
+ */
+
+
+@IgnoreExtraProperties
+public class Score {
+    private String nickname;
+    private String score;
+
+
+    public Score(String nickname, String score) {
+        this.nickname = nickname;
+        this.score  = score;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getScore() {
+        return score;
+    }
+}
