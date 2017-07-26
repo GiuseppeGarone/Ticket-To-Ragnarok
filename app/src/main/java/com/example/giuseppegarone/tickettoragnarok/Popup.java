00d6046bf4dd23d773d088c6ffca436fe6e6febb
melissacoarezza <s231688@studenti.polito.it>
2017-07-26 17:55:56 +0200

    Database Q/A fatto

--- app/src/main/java/com/example/giuseppegarone/tickettoragnarok/Popup.java
+++ app/src/main/java/com/example/giuseppegarone/tickettoragnarok/Popup.java
@@ -6,11 +6,24 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static android.content.ContentValues.TAG;
 
 public class Popup extends Activity {
 
@@ -27,6 +40,9 @@ public class Popup extends Activity {
     public String rispGiusta = "";         // Testo risposta scelta
     public TextView testoDomanda;          // Domanda
 
+    //our database reference object
+    DatabaseReference databaseDomande;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +50,9 @@ public class Popup extends Activity {
 
         // Orientamento landscape
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //getting the reference of Domande node
+        databaseDomande = FirebaseDatabase.getInstance().getReference("Domande");
 
         // Setto dimensioni popup
         DisplayMetrics dm = new DisplayMetrics();
@@ -58,12 +77,46 @@ public class Popup extends Activity {
         provaIntent.setText(Integer.toString(tmpPunti));
         */
 
-        // Memorizzo le risposte
+        /* Memorizzo le risposte
         risp1Testo = risp1.getText().toString();
         risp2Testo = risp2.getText().toString();
         risp3Testo = risp3.getText().toString();
         risp4Testo = risp4.getText().toString();
-        rispGiusta = "17 Gennaio 1992";
+        rispGiusta = "17 Gennaio 1992";*/
+
+        databaseDomande.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                int n = (int)dataSnapshot.getChildrenCount();
+
+                Random rand = new Random();
+                int i = rand.nextInt(n);
+
+                final ArrayList<Question> objects = new ArrayList<>();
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    Question object = child.getValue(Question.class);
+                    objects.add(object);
+                }
+                Question Dscelta = objects.get(i);
+                // Memorizzo le risposte
+                testoDomanda.setText(Dscelta.getDtext());
+                risp1.setText(Dscelta.getR1());
+                risp2.setText(Dscelta.getR2());
+                risp3.setText(Dscelta.getR3());
+                risp4.setText(Dscelta.getR4());
+                risp1Testo = Dscelta.getR1();
+                risp2Testo = Dscelta.getR2();
+                risp3Testo = Dscelta.getR3();
+                risp4Testo = Dscelta.getR4();
+                rispGiusta = Dscelta.getRgiusta();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         // Listener pulsante CONFERMA RISPOSTA
         confirmButton.setOnClickListener(new View.OnClickListener() {
