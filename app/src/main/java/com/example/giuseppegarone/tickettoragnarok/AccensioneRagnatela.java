4baa7ff011716c326e5832cddc9d7873278022ab
Marco Florian <marco.2994@gmail.com>
2017-08-31 14:23:28 +0200

    accensione led

--- app/src/main/java/com/example/giuseppegarone/tickettoragnarok/AccensioneRagnatela.java
+++ app/src/main/java/com/example/giuseppegarone/tickettoragnarok/AccensioneRagnatela.java
@@ -132,6 +132,8 @@ public class AccensioneRagnatela extends Activity {
         int da = daAccendere.partenza;
         int a = daAccendere.arrivo;
 
+        da = 300;
+        a = 360;
         try {
             JSONArray pixels_array = new JSONArray();
             JSONObject tmp;
